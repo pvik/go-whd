2c722d167bf01f94d869082c94474456bd996889
Praveen Vikram <praveen.vikram@kiosa.org>
2019-06-27 18:50:43 -0400

    updates to ticket

--- whd/ticket.go
+++ whd/ticket.go
@@ -1,6 +1,7 @@
 package whd
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -10,8 +11,9 @@ import (
 )
 
 type ProblemType struct {
-	Id   int
-	Name string `json:"detailDisplayName"`
+	Id   int    `json:"id"`
+	Type string `json:"type"`
+	Name string `json:"detailDisplayName,omitempty"`
 }
 
 type CustomField struct {
@@ -20,22 +22,22 @@ type CustomField struct {
 }
 
 type Note struct {
-	Id             int `json:"id"`
-	Date           time.Time
-	MobileNoteText string
+	Id             int       `json:"id"`
+	Date           time.Time `json:"date"`
+	MobileNoteText string    `json:"mobileNoteText`
 }
 
 type Ticket struct {
-	Id             int
-	Detail         string
-	Subject        string
-	LastUpdated    time.Time
-	LocationId     int
-	StatusTypeId   int
-	PriorityTypeId int
-	ProblemType    ProblemType
-	CustomFields   []CustomField `json:"ticketCustomFields"`
-	Notes          []Note
+	Id             int           `json:"id,omitempty"`
+	Detail         string        `json:"detail"`
+	Subject        string        `json:"subject"`
+	LastUpdated    time.Time     `json:"lastUpdated"`
+	LocationId     int           `json:"locationId"`
+	StatusTypeId   int           `json:"statusTypeId"`
+	PriorityTypeId int           `json:"priorityTypeId"`
+	ProblemType    ProblemType   `json:"problemtype"`
+	CustomFields   []CustomField `json:"ticketCustomFields,omitempty"`
+	Notes          []Note        `json:"notes,omitempty"`
 }
 
 func GetTicket(uri string, user User, id int, ticket *Ticket) error {
@@ -61,4 +63,75 @@ func GetTicket(uri string, user User, id int, ticket *Ticket) error {
 	}
 
 	return nil
+}
+
+func CreateUpdateTicket(uri string, user User, whdTicket Ticket) (int, error) {
+	whdTicketMap := make(map[string]interface{})
+	interim, _ := json.Marshal(whdTicket)
+	json.Unmarshal(interim, &whdTicketMap)
+
+	delete(whdTicketMap, "lastUpdated")
+	whdTicketMap["customFields"] = whdTicketMap["ticketCustomFields"]
+	delete(whdTicketMap, "ticketCustomFields")
+
+	ticketJsonStr, _ := json.Marshal(whdTicketMap)
+	if whdTicket.Id == 0 {
+		return createTicket(uri, user, []byte(ticketJsonStr))
+	} else {
+		return updateTicket(uri, user, whdTicket.Id, []byte(ticketJsonStr))
+	}
+}
+
+func createTicket(uri string, user User, ticketJsonStr []byte) (int, error) {
+	req, err := http.NewRequest("POST", uri+urn+"Ticket", bytes.NewBuffer(ticketJsonStr))
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	WrapAuth(req, user)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("The HTTP request failed with error %s\n", err)
+		return 0, err
+	}
+
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	var ticket Ticket
+	if err = json.Unmarshal(data, &ticket); err != nil {
+		log.Println("error unmarshalling: ", err)
+		return 0, err
+	}
+
+	return ticket.Id, nil
+}
+
+func updateTicket(uri string, user User, id int, ticketJsonStr []byte) (int, error) {
+	req, err := http.NewRequest("PUT", uri+urn+"Ticket/"+strconv.Itoa(id), bytes.NewBuffer(ticketJsonStr))
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	WrapAuth(req, user)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("The HTTP request failed with error %s\n", err)
+		return 0, err
+	}
+
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	var ticket Ticket
+	if err = json.Unmarshal(data, &ticket); err != nil {
+		log.Println("error unmarshalling: ", err)
+		return 0, err
+	}
+
+	return ticket.Id, nil
 }
