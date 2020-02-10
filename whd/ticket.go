ce0819c938e86b4be74292260788ccae86c83a44
Praveen Vikram <praveen.vikram@kiosa.org>
2020-02-10 14:35:42 -0500

    att fix

--- whd/ticket.go
+++ whd/ticket.go
@@ -530,7 +530,7 @@ func UploadAttachmentToEntity(uri string, user User, entity string, entityId int
 	log.Println("writer: %+v", writer)
 
 	//log.Printf("Body: %+v", body)
-	postUrl := fmt.Sprintf("%s%s/helpdesk/attachment/upload?type=%s&entityId=%d&returnFields=id", uri, urn, entity, entityId)
+	postUrl := fmt.Sprintf("%s%s/helpdesk/attachment/upload?type=%s&entityId=%d&returnFields=id&sessionKey=%s", uri, urn, entity, entityId, sessionKey)
 	log.Printf("Sending Attachment POST to: %s", postUrl)
 	req2, err := http.NewRequest("POST", postUrl, body)
 	if err != nil {
