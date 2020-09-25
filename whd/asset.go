c110706fa9f62aff9286bc5e7bd9ab8b79f9ac59
Praveen Vikram <praveen.vikram@kiosa.org>
2020-09-25 16:14:11 -0400

    feat: allow Assets endpoints

---
+++ whd/asset.go
@@ -0,0 +1,60 @@
+package whd
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func GetAssets(uri string, user User, qualifier string, limit uint, page uint, asset *[]Asset, sslVerify bool) error {
+	req, err := http.NewRequest("GET", uri+urn+"Assets", nil)
+	if err != nil {
+		return err
+	}
+
+	WrapAuth(req, user)
+
+	if limit == 0 {
+		limit = 25
+	} else if limit > 100 {
+		limit = 100
+	}
+
+	if page == 0 {
+		page = 1
+	}
+
+	q := req.URL.Query()
+	q.Add("qualifier", qualifier)
+	q.Add("limit", strconv.FormatUint(uint64(limit), 10))
+	q.Add("page", strconv.FormatUint(uint64(page), 10))
+	req.URL.RawQuery = q.Encode()
+
+	var client *http.Client
+	if !sslVerify {
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client = &http.Client{Transport: tr}
+	} else {
+		client = &http.Client{}
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("The HTTP request failed with error %s\n", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	if err = json.Unmarshal(data, &asset); err != nil {
+		log.Println("error unmarshalling: ", err)
+		return err
+	}
+
+	return nil
+}
